0dea93cc9e221c613536b0e26557391440b6787d
Renato Cruz <renato.kim@hotmail.com>
2021-03-14 14:50:19 -0300

    first commit

---
+++ NotaFiscalClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NotaFiscal
+{
+    public class NotaFiscalClass
+    {
+        public event EventHandler OnEmitir;
+
+        public void Emitir()
+        {
+            if (OnEmitir != null)
+            {
+                OnEmitir(this, new EventArgs());
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NotaFiscal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                var randon = new Random().Next(8);
+                Console.WriteLine(randon);
+            }
+
+
+
+
+            var nf = new NotaFiscalClass();
+
+            nf.OnEmitir += EnviarEmail;
+            nf.OnEmitir += EmitirSinal;
+            nf.OnEmitir += EmitirSinal;
+
+            nf.Emitir();
+
+            //nf.OnEmitir();
+
+
+
+
+
+
+        }
+
+        public static void EnviarEmail(object sender, EventArgs args)
+        {
+            Console.WriteLine("Enviando email....");
+        }
+
+        public static void EmitirSinal(object sender, EventArgs args)
+        {
+            Console.WriteLine("Emitindo sinal....");
+        }
+    }
+}
